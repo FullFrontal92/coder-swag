3c115dc31a8fd1ed97ab59df8d4aafc7462d0cf5
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-02-25 20:39:47 +0100

    controller for products add

---
+++ coder-swag/Controller/ProductsVC.swift
@@ -0,0 +1,35 @@
+//
+//  ProductVC.swift
+//  coder-swag
+//
+//  Created by Adam Benyahia on 25/02/2018.
+//  Copyright © 2018 Adam Benyahia. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
